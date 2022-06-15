7c7a1b6c8c9a1df004e36813afcc1ddff872412d
Ali Ghodrat <ali@nethire.dk>
2022-06-15 14:58:48 +0200

    add logic, final commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+class Program
+{
+    private static void Main()
+    {
+        Random random = new Random();
+        string[] orderIDs = new string[5];
+
+        for (int i = 0; i < orderIDs.Length; i++)
+        {
+            int prefixValue = random.Next(65, 70);
+            string prefix = Convert.ToChar(prefixValue).ToString();
+            string suffix = random.Next(1, 1000).ToString("000");
+
+            orderIDs[i] = prefix + suffix;
+        }
+
+        foreach (var orderID in orderIDs)
+        {
+            Console.WriteLine(orderID);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fraud-detection
 The following code creates five random IDs to test the fraud detection process.
+
+Credit:
+
+*MS Exercise - Comment your code*
+
+https://docs.microsoft.com
+
+  The following code creates five random IDs
+  to test the fraud detection process. IDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123. (changed OrderIDs to IDs)
